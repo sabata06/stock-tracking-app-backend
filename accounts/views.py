955ba74a3dbc4a01f9d1bcf0f5e8ca6febd1e711
sabata06 <m.karaarslan06@outlook.com>
2025-03-10 01:28:23 +0300

    ``` No code changes made. ```

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .models import CustomUser
 from .serializers import UserRegistrationSerializer
 from .serializers import PasswordChangeSerializer
 from .serializers import UserStatusUpdateSerializer
 
+
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]  # Bu satırı ekleyin
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
